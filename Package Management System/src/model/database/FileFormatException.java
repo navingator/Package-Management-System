ebc22922322fabcda0bc9bb6d39f1a40b7f3acd7
Navin <np4navin@gmail.com>
2014-03-13 17:05:58 -0500

    Added FileFormatException

---
+++ Package Management System/src/model/database/FileFormatException.java
@@ -0,0 +1,13 @@
+package model.database;
+
+public class FileFormatException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3328919630430592198L;
+	
+	public FileFormatException() { super(); }
+	public FileFormatException(String message) { super(message); }
+	
+}
