34de865dbf2f5f3c9376aa005efd63385fad690e
Navin <np4navin@gmail.com>
2014-04-06 22:31:25 -0500

    Removed unused imports.

--- Package Management System/src/view/panel/PanelPickUp.java
+++ Package Management System/src/view/panel/PanelPickUp.java
@@ -2,9 +2,6 @@ package view.panel;
 
 import java.awt.Font;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -27,7 +24,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.File;
 
 public class PanelPickUp extends JPanel {
 
