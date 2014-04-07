2bb394c50e4d091d1c7bab8fd588d9802d9f3dfa
Navin <np4navin@gmail.com>
2014-04-07 11:32:01 -0500

    Added notification sent flag to package

--- Package Management System/src/util/Package.java
+++ Package Management System/src/util/Package.java
@@ -10,6 +10,7 @@ public class Package {
 	private String comment;
 	private final Date checkInDate;
 	private Date checkOutDate;
+	private boolean notificationSent;
 	
 	
 	public Package(long packageID, String comment, Date checkInDate) {
@@ -26,17 +27,19 @@ public class Package {
 	 *  comment
 	 *  checkInDate
 	 *  checkOutDate
+	 *  notificationSent
 	 *  
 	 * set:
 	 * 	comment
 	 * 	checkOutDate
+	 * 	notificationSent
 	 */
 	
 	public Date getCheckOutDate() {
 		return checkOutDate;
 	}
-	public void setCheckOutDate(Date checkOutDate) {
-		this.checkOutDate = checkOutDate;
+	public boolean isNotificationSent() {
+		return notificationSent;
 	}
 	public long getPackageID() {
 		return packageID;
@@ -44,10 +47,17 @@ public class Package {
 	public String getComment() {
 		return comment;
 	}
+	public Date getCheckInDate() {
+		return checkInDate;
+	}
+	
+	public void setCheckOutDate(Date checkOutDate) {
+		this.checkOutDate = checkOutDate;
+	}
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
-	public Date getCheckInDate() {
-		return checkInDate;
+	public void setNotificationSent(boolean notificationSent) {
+		this.notificationSent = notificationSent;
 	}
 }
